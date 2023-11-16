aff51391934c2008233332599f3573cce2ec60c0
Michela Vezzoli <107128217+mikivezzoli@users.noreply.github.com>
2023-11-16 10:06:51 +0100

    Update AMPposneg.js

--- AMPposneg.js
+++ AMPposneg.js
@@ -1,54 +1,46 @@
-define(['pipAPI', 'https://github.com/mikivezzoli/AMP/blob/main/AMP.js'], function(APIConstructor, ampExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualtrics/qamp.js'], function(APIConstructor, ampExtension){
 
 	var API = new APIConstructor();
-	var global = API.getGlobal();
+	
 	
 	return ampExtension({
 		primeCats :  [
 			{
-				nameForFeedback : 'positive',  //Will be used in the user feedback 
-				nameForLogging : 'positive', //Will be used in the logging
+				nameForFeedback : 'Black people',  //Will be used in the user feedback 
+				nameForLogging : 'Black people', //Will be used in the logging
 				//An array of all media objects for this category.
 				mediaArray : [
-                    {image: 'pos1.jpg'},
-                    {image: 'pos2.jpg'},
-                    {image: 'pos3.jpg'},
-                    {image: 'pos4.jpg'},
-                    {image: 'pos5.jpg'},
-                    {image: 'pos6.jpg'},
-                    {image: 'pos7.jpg'},
-                    {image: 'pos8.jpg'},
-                    {word: 'Paradise'},
-                    {word: 'Pleasure'},
-                    {word: 'Cheer'},
-                    {word: 'Splendid'},
-                    {word: 'Love'},
-                    {word: 'Smile'},
-                    {word: 'Friend'},
-                    {word: 'Enjoy'}
-        		]
+				    {image : 'b01.jpg'}, 
+					{image : 'b02.jpg'}, 
+					{image : 'b03.jpg'}, 
+					{image : 'b04.jpg'}, 
+					{image : 'b05.jpg'}, 
+					{image : 'b06.jpg'}, 
+					{image : 'b07.jpg'}, 
+					{image : 'b08.jpg'}, 
+					{image : 'b09.jpg'}, 
+					{image : 'b10.jpg'}, 
+					{image : 'b11.jpg'}, 
+					{image : 'b12.jpg'}]
+
 			}, 
 			{
-				nameForFeedback : 'negative',  //Will be used in the user feedback 
-				nameForLogging : 'negative', //Will be used in the logging
+				nameForFeedback : 'White people',  //Will be used in the user feedback 
+				nameForLogging : 'White people', //Will be used in the logging
 				//An array of all media objects for this category.
 				mediaArray : [
-                    {image: 'neg1.jpg'},
-                    {image: 'neg2.jpg'},
-                    {image: 'neg3.jpg'},
-                    {image: 'neg4.jpg'},
-                    {image: 'neg5.jpg'},
-                    {image: 'neg6.jpg'},
-                    {image: 'neg7.jpg'},
-                    {image: 'neg8.jpg'},
-                    {word: 'Bomb'},
-                    {word: 'Abuse'},
-                    {word: 'Sadness'},
-                    {word: 'Pain'},
-                    {word: 'Grief'},
-                    {word: 'Ugly'},
-                    {word: 'Poison'},
-                    {word: 'Stink'}]
+				    {image : 'w01.jpg'}, 
+					{image : 'w02.jpg'}, 
+					{image : 'w03.jpg'}, 
+					{image : 'w04.jpg'}, 
+					{image : 'w05.jpg'}, 
+					{image : 'w06.jpg'}, 
+					{image : 'w07.jpg'}, 
+					{image : 'w08.jpg'}, 
+					{image : 'w09.jpg'}, 
+					{image : 'w10.jpg'}, 
+					{image : 'w11.jpg'}, 
+					{image : 'w12.jpg'}]
 			}
 		],
 
@@ -56,7 +48,6 @@ define(['pipAPI', 'https://github.com/mikivezzoli/AMP/blob/main/AMP.js'], functi
 		{
 			nameForLogging : 'examplePrime', //Will be used in the logging
 			//An array of all media objects for this category.
-			//mediaArray : [{word : 'Table'}, {word : 'Chair'}]
 			mediaArray : [{image : 'ampchair.jpg'}, {image : 'amplamp.jpg'}, {image : 'ampumbrella.jpg'}]
 		},
 
